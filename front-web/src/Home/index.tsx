7be7a92f3e066fe9e16c4d2f3f463add16d13299
Dacinho <dacio.bezerra@ccc.ufcg.edu.br>
2021-01-08 10:34:26 -0300

    Adding Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,27 +1,38 @@
 import "./styles.css";
-import {ReactComponent as HomeImage} from "./assets/home-image.svg";
+import { ReactComponent as HomeImage } from "./assets/home-image.svg";
+import Footer from "../Footer";
 
 function Home() {
-    return (
-        <div className="home-container">
-            <div className="home-content">
-                <div className="home-actions">
-                    <h1 className="home-title">
-                        Faça seu pedido<br/>que entregamos<br/>para você!!!
-                    </h1>
-                    <h3 className="home-subtitle">
-                        Escolha o seu pedido e em poucos minutos<br/>levaremos na sua porta
-                    </h3>
-                    <a className="home-btn-order" href="orders">
-                        FAZER PEDIDO
-                    </a>
-                </div>
-                <div className="home-image">
-                    <HomeImage />
-                </div>
-            </div>
+  return (
+    <>
+      <div className="home-container">
+        <div className="home-content">
+          <div className="home-actions">
+            <h1 className="home-title">
+              Faça seu pedido
+              <br />
+              que entregamos
+              <br />
+              para você!!!
+            </h1>
+            <h3 className="home-subtitle">
+              Escolha o seu pedido e em poucos minutos
+              <br />
+              levaremos na sua porta
+            </h3>
+            <a className="home-btn-order" href="orders">
+              FAZER PEDIDO
+            </a>
+          </div>
+          <div className="home-image">
+            <HomeImage />
+          </div>
         </div>
-    );
+        
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
