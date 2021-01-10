a7efe0be157021bf93171f352209e98ae079a65f
Dacinho <dacio.bezerra@ccc.ufcg.edu.br>
2021-01-09 23:06:28 -0300

    Adding env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
-const API_URL = "https://devsuper.herokuapp.com";
+const API_URL = process.env.REACT_APP_API_URL//"https://devsuper.herokuapp.com";
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAPBOX;
 
 export function fetchProducts() {
